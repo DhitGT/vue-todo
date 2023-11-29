6a4a16f209d96f211442a61bf0b2ad5dd894c2fc
DhitGT <adityadwisaputra666@gmail.com>
2023-11-29 19:12:58 +0700

    add register modal

--- components/Forms/Login.vue
+++ components/Forms/Login.vue
@@ -21,7 +21,7 @@ async function signIn() {
 
 <template>
   <form @submit.prevent="() => signIn()" class="max-w-sm mx-auto">
-    <p>{{ info }}</p>
+    <p class="text-red-600">{{ info }}</p>
     <div class="mb-5">
       <label
         for="email"
@@ -56,6 +56,7 @@ async function signIn() {
       type="submit"
       @click="signIn()"
       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      style="width: 100%"
     >
       Login
     </button>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <h1 align="center">Login</h1>
-  <FormsLogin />
-  <br />
-  <p align="center">or</p>
-  <br />
-  <br />
-  <h1 align="center">Register</h1>
-  <FormsRegister />
+  <div
+    class="block max-w-sm p-6 m-auto mt-36 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+  >
+    <h1 align="center">Login</h1>
+    <FormsLogin />
+    <br />
+    <hr />
+    <br />
+    <div
+      class="text-sm flex font-medium text-gray-500 dark:text-gray-300 text-center"
+    >
+      Not registered?
+      <button
+        data-modal-target="Register-modal"
+        data-modal-toggle="Register-modal"
+        class="block w-max ms-1 text-white bfont-medium rounded-lg text-sm text-center hover:underline"
+        type="button"
+      >
+        Register
+      </button>
+      <FormsRegister />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 definePageMeta({
   layout: "register",
+});
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
 });
 </script>
